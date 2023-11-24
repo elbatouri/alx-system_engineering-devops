1e3ec07ea4d77bf0f8c7610bedc9b43f559a9626
elbatouri rabi <elbatouri@gmail.com>
2023-11-24 22:58:21 +0000

    still trying with the 1 task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
 # puppet declarative script to install flask from pip3.
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
+
