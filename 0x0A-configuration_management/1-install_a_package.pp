2180e0e825d6386eab05b1371aabbef3ad8dbdc9
elbatouri rabi <elbatouri@gmail.com>
2023-11-24 22:47:49 +0000

    test

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# installing a package using puppet
+# puppet declarative script to install flask from pip3.
 package { 'flask':
-  ensure   => '.1.0',
-  provider => 'ip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
