5c8835941a504341b42c86824b4059a202b47157
elbatouri rabi <elbatouri@gmail.com>
2024-01-02 16:38:30 -0800

    fiw the 3th task

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+export data in the JSON format.
+
+Requirements:
+
+Records all tasks from all employees
+File name must be: todo_all_employees.json
+"""
+import json
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            user.get("id"): [{
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": todo.get("username")
+                } for todo in requests.get(url + "todos",
+                    params={"userId": user.get("id")}).json()]
+            for user in users}, jsonfile)
