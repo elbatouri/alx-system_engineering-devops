00da31f48ef48e598fab45a0b22bc62dc7ff6cab
EL BATOURI Rabi <elbatouri@gmail.com>
2024-01-16 12:17:48 -0800

    fix the client id

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """
-This function queries Reddit API and
-retrieves the number of subscribers for a subreddit.
+queries redditapi and return subscribers number in subbreddit
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Function to return the number of subscribers"""
-    user = {'User-Agent': 'elbatouri'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = requests.get(url, headers=user)
+    user_agent = 'elbatouri/1.0'
+    client_id = 'WqSQ8WwNC9Y6ixCB1Gx9sA'
+    headers = {'User-Agent': user_agent,
+               'Authorization': f'Client-ID {client_id}'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
 
-    print(f"Status Code: {response.status_code}")
-    print(f"Response Content: {response.text}")
-
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('data').get('subscribers')
-    else:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            return response.json()['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException as e:
+        print(f"Error: {e}")
         return 0
