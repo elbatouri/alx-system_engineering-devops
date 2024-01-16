ea3a950d82e6ec41da419ed0aa57d9c1d6233300
EL BATOURI Rabi <elbatouri@gmail.com>
2024-01-16 13:56:25 -0800

    task 2 done

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+"""
+
+import requests
+
+def recurse(subreddit, hot_list=[], after=None):
+    
+    user_agent = 'elbatouri/1.0'
+    client_id = 'WqSQ8WwNC9Y6ixCB1Gx9sA'
+    headers = {'User-Agent': user_agent,
+               'Authorization': f'Client-ID {client_id}'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    params = {'limit': 100, 'after': after}
+
+    if response.status_code != 200:
+        return None
+
+    data = response.json().get('data', {})
+    children = data.get('children', [])
+
+    for child in children:
+        hot_list.append(child['data']['title'])
+
+    after = data.get('after')
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after)
+    else:
+        return hot_list
