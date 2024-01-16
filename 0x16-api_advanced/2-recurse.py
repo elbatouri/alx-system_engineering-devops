88ea1bd2ed35274e4c5d08be832e066f39765632
EL BATOURI Rabi <elbatouri@gmail.com>
2024-01-16 14:25:16 -0800

    fix pycodestyle issue on file 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns a list containing the titles 
-of all hot articles for a given subreddit. If no results are found 
+Queries the Reddit API and returns a list containing the titles
+of all hot articles for a given subreddit. If no results are found
 for the given subreddit, the function should return None.
 """
 
 import requests
+
 
 def recurse(subreddit, hot_list=[], after=None):
     user_agent = 'elbatouri/1.0'
     client_id = 'WqSQ8WwNC9Y6ixCB1Gx9sA'
     headers = {'User-Agent': user_agent,
                'Authorization': f'Client-ID {client_id}'}
-    
+
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     params = {'limit': 100, 'after': after}
 
     try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        response = requests.get(url, headers=headers,
+                                params=params, allow_redirects=False)
 
         if response.status_code == 200:
             data = response.json().get('data', {})
