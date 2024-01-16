d17c91d3d98b6ee52487dc22417898d740701341
EL BATOURI Rabi <elbatouri@gmail.com>
2024-01-16 08:03:03 -0800

    fix 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
 """
+This function queries Reddit API and retrieves the number of subscribers for a subreddit.
 """
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    url=requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user).json()
-    headers = {'User-Agent': 'elbarabi'}
-    response = requests.get(url, headers=headers)
+    """Function to return the number of subscribers"""
+    user = {'User-Agent': 'elbatouri'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = requests.get(url, headers=user)
+
+    print(f"Status Code: {response.status_code}")
+    print(f"Response Content: {response.text}")
+
     if response.status_code == 200:
         data = response.json()
         return data.get('data').get('subscribers')
     else:
         return 0
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        subscribers = number_of_subscribers(subreddit)
+        print(subscribers)
+
