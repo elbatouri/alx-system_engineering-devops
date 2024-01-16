a46d01e0672a3426198dc92ad98ac7ac29bda971
EL BATOURI Rabi <elbatouri@gmail.com>
2024-01-16 14:00:30 -0800

    fix the comment on file 2-rescue

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 """
+queries the Reddit API and returns a list containing the titles 
+of all hot articles for a given subreddit. If no results are found 
+for the given subreddit, the function should return None.
 """
 
 import requests
